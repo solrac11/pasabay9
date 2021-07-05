5f00802ef0bb051848dd8d03c15805740a0125ec
Carlos Melegrito <caltech511@gmail.com>
2021-07-06 06:31:48 +0800

    1st attempt of deployment on heroku

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 require ("dotenv").config({ path: "../config.env" });
+const path = require('path');
 const express = require("express");
 const connectDB = require("./config/db");
 const productRoutes = require("./routes/productRoutes");
@@ -11,6 +12,13 @@ app.use(express.json());
 
 app.use("/api/products", productRoutes);
 
+app.use(express.static(path.join(__dirname, '../frontend/build')));
+
+app.get('*', (req, res) =>{
+    res.sendFile(Path2D.join(__dirname, 'frontend', 'build', 'index.html'));
+})
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
